5ab58c8386548f588da080ca478de9ee3cf461a2
Ricardo Monterrosa <rmonterrosa@lji.org>
2022-03-07 11:14:17 -0800

    create relationships between content

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,5 +1,8 @@
 <?php 
     function university_post_types() {
+
+        // Event Post Type
+
         register_post_type('event', array(
             'supports' => array('title', 'editor', 'excerpt'),
             'rewrite' => array('slug' => 'events'),
@@ -15,7 +18,26 @@
             ),
             'menu_icon' => 'dashicons-calendar-alt'
         ));
+
+        // Program Post Type
+
+        register_post_type('program', array(
+            'supports' => array('title', 'editor'),
+            'rewrite' => array('slug' => 'programs'),
+            'has_archive' => true,
+            'public' => true,
+            'show_in_rest' => true,
+            'labels' => array(
+                'name' => 'Programs',
+                'add_new_item' => 'Add New Program',
+                'edit_item' => 'Edit Program',
+                'all_items' => 'All Programs',
+                'singular_name' => 'Program'
+            ),
+            'menu_icon' => 'dashicons-awards'
+        ));
     }
 
     add_action('init', 'university_post_types');
 ?>
+
--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -22,18 +22,24 @@
         $today = date('Ymd');
 
         if (!is_admin() && is_post_type_archive('event') && is_main_query()) {
-        $query->set('meta_key', 'event_date');
-        $query->set('orderby', 'meta_value_num');
-        $query->set('order', 'ASC');
-        $query->set('meta_query', array(
-            array(
-                'key' => 'event_date',
-                'compare' => '>=',
-                'value' => $today,
-                'type' => 'numeric'
-            )
-        ));
-        }
+            $query->set('meta_key', 'event_date');
+            $query->set('orderby', 'meta_value_num');
+            $query->set('order', 'ASC');
+            $query->set('meta_query', array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '>=',
+                    'value' => $today,
+                    'type' => 'numeric'
+                )
+            ));
+        };
+
+        if (!is_admin() && is_post_type_archive('program') && is_main_query()) {
+            $query->set('orderby', 'title');
+            $query->set('order', 'ASC');
+            $query->set('posts_per_page', -1);
+        };
     }
 
     add_action( 'pre_get_posts', 'university_adjust_queries');
