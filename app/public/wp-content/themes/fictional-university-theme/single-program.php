b3ee5e3f9f9bbecef273a7dd569dba694fbb54c3
Ricardo Monterrosa <rmonterrosa@lji.org>
2022-03-14 15:38:18 -0700

    custom image sizes

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -32,6 +32,37 @@
     </div>
 
     <?php
+        $relatedProfessors = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'professor',
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'meta_query' => array(
+                array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . get_the_ID() . '"'
+                )
+            )
+        ));
+
+        if ($relatedProfessors->have_posts()) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professor(s)</h2>';
+
+            echo '<ul class="professor-cards">';
+            while($relatedProfessors->have_posts()) {
+                $relatedProfessors->the_post(); ?>
+                <li class="professor-card__list-item">
+                    <a class="professor-card" href="<?php the_permalink() ?>">
+                        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="professor-card__image">
+                        <span class="professor-card__name"><?php the_title(); ?></span>
+                    </a>
+                </li>
+            <?php } wp_reset_postdata();
+            echo '</ul>';
+        }   
+
         $today = date('Ymd');
 
         $homepageEvents = new WP_Query(array(
@@ -61,30 +92,30 @@
 
             while($homepageEvents->have_posts()) {
                 $homepageEvents->the_post(); ?>
-                <div class="event-summary">
-                    <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
-                        <span class="event-summary__month"><?php 
+    <div class="event-summary">
+        <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
+            <span class="event-summary__month"><?php 
                                     $eventDate = new DateTime(get_field('event_date'));
                                     echo $eventDate->format('M');
                                 ?></span>
-                        <span class="event-summary__day"><?php 
+            <span class="event-summary__day"><?php 
                                     echo $eventDate->format('d');
                                 ?></span>
-                    </a>
-                    <div class="event-summary__content">
-                        <h5 class="event-summary__title headline headline--tiny"><a
-                                href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                        <p>
-                            <?php if(has_excerpt()) {
+        </a>
+        <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a
+                    href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+            <p>
+                <?php if(has_excerpt()) {
                                         echo get_the_excerpt();
                                     } else {
                                         echo wp_trim_words(get_the_content(), 18);
                                     } ?>
-                            <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a>
-                        </p>
-                    </div>
-                </div>
-            <?php } wp_reset_postdata();
+                <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a>
+            </p>
+        </div>
+    </div>
+    <?php } wp_reset_postdata();
         }
         
     ?>
